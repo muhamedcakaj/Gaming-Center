c8588d181cedb540921cc33f8a3dc7d97403a311
muhamedcakaj <muhamedcakaj57@gmail.com>
2025-12-18 21:45:35 +0100

    Api Response Fix

--- src/services/pcStatusService.ts
+++ src/services/pcStatusService.ts
@@ -1,9 +1,12 @@
 import type { PcStatus } from "../types/PcStatus"; 
-const API_URL = "https://gamingcenter-3.onrender.com/"; 
+const API_URL = "https://gamingcenter.onrender.com/getAll"; 
 export const getBusyPcs = async (): Promise<PcStatus[]> => { 
 const response = await fetch(API_URL); 
 if (!response.ok) {
  throw new Error("Failed to fetch PC status");
- } 
-return response.json(); 
+ }
+
+
+return response.json();
+
 };
